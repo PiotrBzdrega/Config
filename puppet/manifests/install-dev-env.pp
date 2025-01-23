e3530c8a38938d72ab75b0936eeb8cf6f55d4880
PiotrBzdrega <45517077+PiotrBzdrega@users.noreply.github.com>
2025-01-23 23:49:54 +0100

    maven update url
    Signed-off-by: PiotrBzdrega <45517077+PiotrBzdrega@users.noreply.github.com>

--- puppet/manifests/install-dev-env.pp
+++ puppet/manifests/install-dev-env.pp
@@ -21,7 +21,7 @@ node 'dev-box' {
 
 	# Maven is used as dependency management and build tool.
 	exec { 'maven':
-		command => "/usr/bin/wget -q -P /home/dev/Tools https://dlcdn.apache.org/maven/maven-3/${mavenVersion}/binaries/apache-maven-${mavenVersion}-bin.tar.gz && /bin/tar xzf /home/dev/Tools/apache-maven-${mavenVersion}-bin.tar.gz -C /home/dev/Tools && /bin/ln -s /home/dev/Tools/apache-maven-${mavenVersion}/bin/mvn /usr/bin/mvn",
+		command => "/usr/bin/wget -q -P /home/dev/Tools https://archive.apache.org/dist/maven/maven-3/${mavenVersion}/binaries/apache-maven-${mavenVersion}-bin.tar.gz && /bin/tar xzf /home/dev/Tools/apache-maven-${mavenVersion}-bin.tar.gz -C /home/dev/Tools && /bin/ln -s /home/dev/Tools/apache-maven-${mavenVersion}/bin/mvn /usr/bin/mvn",
 		returns => [0, 4],
 	}
 
